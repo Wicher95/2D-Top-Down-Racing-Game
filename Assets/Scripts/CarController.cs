75b8fbcc69d9e3c32115380c04c24803ea59c900
Wicher <wicher.1995@gmail.com>
2019-07-19 20:54:43 +0200

    Initial commit

---
+++ Assets/Scripts/CarController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : MonoBehaviour
+{
+    public float carHorsePower;
+    float speed, direction, steeringAmount;
+    Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        steeringAmount = -Input.GetAxis("Horizontal");
+        speed = Input.GetAxis("Vertical") * carHorsePower;
+        direction = Mathf.Sign(Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up)));
+        rb.rotation += steeringAmount * rb.velocity.magnitude * direction;
+
+        rb.AddRelativeForce(Vector2.up * speed);
+
+        rb.AddRelativeForce(-Vector2.right * rb.velocity.magnitude * steeringAmount / 2);
+    }
+}
