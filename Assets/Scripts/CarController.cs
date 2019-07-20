c73d5d6b36005a2f6a60e13fe2cce3253b49fc6d
Wicher <wicher.1995@gmail.com>
2019-07-20 12:03:38 +0200

    Much Better Car Controls | Trails | HandBrake

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -7,12 +7,20 @@ public class CarController : MonoBehaviour
     public static CarController instance;
 
     public float carHorsePower;
-    float speed, direction, steeringAmount;
+    public TrailRenderer[] trails;
+
+    float torque = -150f;
+    float speed, steeringAmount;
+    float driftSticky = 0.6f;
+    float driftSlippy = 1f;
+    float maxStickyVelocity = 0.7f;
+    bool inDrift;
     Rigidbody2D rb;
 
     private void Awake()
     {
         instance = this;
+        inDrift = false;
     }
 
     void Start()
@@ -20,17 +28,65 @@ public class CarController : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
     }
 
+    private void Update()
+    {
+        if(inDrift)
+        {
+            for (int i = 0; i < trails.Length; i++)
+            {
+                trails[i].enabled = true;
+            }
+        }
+        else
+        {
+            for (int i = 0; i < trails.Length; i++)
+            {
+                trails[i].enabled = false;
+            }
+        }
+    }
+
     void FixedUpdate()
     {
-        steeringAmount = -Input.GetAxis("Horizontal");
-        speed = Input.GetAxis("Vertical") * carHorsePower;
-        direction = Mathf.Sign(Vector2.Dot(rb.velocity, rb.GetRelativeVector(Vector2.up)));
-        rb.rotation += steeringAmount * rb.velocity.magnitude * direction;
+        if (Input.GetButton("Accelerate"))
+        {
+            rb.AddForce(transform.up * carHorsePower);
+        }
+        else if(Input.GetButton("Break"))
+        {
+            rb.AddForce(-transform.up * carHorsePower / 2);
+        }
 
-        rb.AddRelativeForce(Vector2.up * speed);
+        steeringAmount = IsDrivingForward() ? Input.GetAxis("Horizontal") : -Input.GetAxis("Horizontal");
 
-        rb.AddRelativeForce(-Vector2.right * rb.velocity.magnitude * steeringAmount / 2);
+        float t = Mathf.Lerp(0, torque, rb.velocity.magnitude);
+        rb.angularVelocity = steeringAmount * t;
+
+
+        float driftFactor = RightVelocty().magnitude > maxStickyVelocity ? driftSlippy : driftSticky;
+        if (Input.GetButton("HandBreak")) { driftFactor = driftSlippy; rb.drag = 3; }
+        else rb.drag = 1;
+        rb.velocity = ForwardVelocity() + RightVelocty() * driftFactor;
+
+        inDrift = driftFactor == driftSlippy ? true : false;
     }
+
+    private Vector2 ForwardVelocity()
+    {
+        // Dot - How much of the velocity is 'going up'
+        return transform.up * Vector2.Dot(rb.velocity, transform.up);
+    }
+    private Vector2 RightVelocty()
+    {
+        // Dot - How much of the velocity is 'going right'
+        return transform.right * Vector2.Dot(rb.velocity, transform.right);
+    }
+
+    private bool IsDrivingForward()
+    {
+        return Vector2.Dot(transform.up.normalized, rb.velocity.normalized) >= 0 ? true : false; 
+    }
+
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
---
+++ Assets/Scripts/TrailController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// This script is repsonsible to remove the efect that trail on enable is rendered from position when it was last disabled
+public class TrailController : MonoBehaviour
+{
+    private float defaultTime;
+    private TrailRenderer trail;
+
+    private void Awake()
+    {
+        trail = GetComponent<TrailRenderer>();
+        defaultTime = trail.time;
+    }
+
+    private void OnEnable()
+    {
+        Invoke("EnableTrail", 0.1f);
+    }
+
+    private void OnDisable()
+    {
+        trail.time = -1;
+    }
+
+    private void EnableTrail()
+    {
+        trail.time = defaultTime;
+    }
+}
